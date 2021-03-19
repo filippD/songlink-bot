42d2d5c8cc69f7dca198a07ce38890f5b2475c17
YellowPipe <philippdmitrienko@gmail.com>
2021-03-19 12:01:09 +0200

    Fix bot

--- bot/router.rb
+++ bot/router.rb
@@ -3,6 +3,9 @@ require_relative 'commands/null.rb'
 require_relative 'commands/send_song_links.rb'
 
 class Router
+  URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.
+    [a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/x
+
   def call(input)
     command(input).new(input)
   end
@@ -10,18 +13,17 @@ class Router
   private
 
   def command(input)
-    regex = Regexp.new(
-      'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)'
-    )
+    return Commands::Null unless input
+
     case input.chat.type
     when 'private'
-      if input.text&.match(regex)
+      if input.text&.match(URL_REGEX)
         Commands::SendSongLinks
       else 
         Commands::Default
       end
     when 'supergroup', 'group'
-      if input.text&.match(regex)
+      if input.text&.match(URL_REGEX)
         Commands::SendSongLinks
       end
     end || Commands::Null
